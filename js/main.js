3a5d92cea8a0086bbfb07634edd3aac2b13b4dac
khaled311 <k.sarhan311@gmail.com>
2019-10-24 19:13:16 +0200

    Deploy

--- js/main.js
+++ js/main.js
@@ -150,17 +150,6 @@ $(function() {
     e.stopPropagation();
   });
 
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
-  const links = document.querySelectorAll(".nav-links li");
-
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-      link.classList.toggle("fade");
-    });
-  });
-
   // $("#drpDwnMob").on("click", function() {
   //   $(".sideNav .details").slideToggle();
   // });
